0e2b8505223ea310aca6035efb1b77946f798af4
Daniyar <yeskaliyevdan@hotmail.com>
2022-11-27 11:44:05 -0800

    installed dev dependencies and updated scripts

--- 03tut/index.js
+++ 03tut/index.js
@@ -1,1 +1,10 @@
-console.log('tested!')
+const { format } = require('date-fns')
+const { v4: uuid } = require('uuid')
+
+console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'))
+
+console.log('hello')
+
+console.log(uuid())
+
+console.log()
