13de6053531ce8a3badc442e695904283e4af488
Daniyar <yeskaliyevdan@hotmail.com>
2022-12-08 01:49:07 -0800

    changed directory structure

--- server.js
+++ server.js
@@ -1,23 +1,68 @@
-// console.log('PiPi')
-// console.log(global)
-
-const os = require('os')
+require('dotenv').config()
+const express = require('express')
+const app = express()
 const path = require('path')
-const { add, subtract, multiply, divide } = require('./math')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const { logger } = require('./middleware/logEvents')
+const errorHandler = require('./middleware/errorHandler')
+const verifyJWT = require('./middleware/verifyJWT')
+const cookieParser = require('cookie-parser')
+const credentials = require('./middleware/credentials')
+const mongoose = require('mongoose')
+const connectDB = require('./config/dbConn')
+const PORT = process.env.PORT || 3500
 
-console.log(add(2,3))
-console.log(subtract(2,3))
-console.log(multiply(2,3))
-console.log(divide(2,3))
-// console.log(os.type())
-// console.log(os.version())
-// console.log(os.homedir())
+// Connect to MongoDB
+mongoose.set('strictQuery', true)
+connectDB()
 
-// console.log(__dirname)
-// console.log(__filename)
+// custom middleware logger
+app.use(logger)
 
-// console.log(path.dirname(__filename))
-// console.log(path.basename(__filename))
-// console.log(path.extname(__filename))
+// handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials)
 
-// console.log(path.parse(__filename))
+//cross origin resource sharing
+app.use(cors(corsOptions))
+
+app.use(express.urlencoded({ extended: false}))
+
+// built-in middleware fro json
+app.use(express.json())
+
+//middleware for cookies
+app.use(cookieParser())
+
+
+app.use('/', express.static(path.join(__dirname, '/public')))
+
+// routes
+app.use('/', require('./routes/root'))
+app.use('/register', require('./routes/register'))
+app.use('/auth', require('./routes/auth'))
+app.use('/refresh', require('./routes/refresh'))
+app.use('/logout', require('./routes/logout'))
+
+app.use(verifyJWT)
+app.use('/employees', require('./routes/api/employees'))
+
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ error: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
+app.use(errorHandler)
+
+mongoose.connection.once('open', () => {
+    console.log('connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
